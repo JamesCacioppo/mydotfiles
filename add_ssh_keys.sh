5bd59020f02de6e4e0223e7a21b06ddf5da5ab22
James Cacioppo <james.m.cacioppo@gmail.com>
2022-08-03 16:56:48 -0400

    -K flag deprecated and replaced with --apple-use-keychain

--- add_ssh_keys.sh
+++ add_ssh_keys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in `ls ~/.ssh`; do
   if [ `file ~/.ssh/$i | grep "private key" | wc -l` == 1 ]; then
-    ssh-add -K ~/.ssh/$i
+    ssh-add --apple-use-keychain ~/.ssh/$i
   fi
 done
